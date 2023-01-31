7f937b8106a134f071729509ba40c346a1ae805c
dani6666 <danielwz99@gmail.com>
2023-01-31 18:36:17 +0100

    tst commit

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,5 @@ docker build ./reverse-proxy -t dani6666/tictactoe-proxy
 
 
 # docker push dani6666/tictactoe-api 
-# docker push dani6666/tictactoe-ui
+# docker push dani6666/tictactoe-ui 
 # docker push dani6666/tictactoe-proxy 
