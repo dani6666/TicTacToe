130926c90489f56e34d825dd70e63c3e0435dce1
dani6666 <danielwz99@gmail.com>
2023-01-31 20:50:39 +0100

    update

--- build-images.sh
+++ build-images.sh
@@ -3,6 +3,6 @@ sudo docker build ./TicTacToeFront -t dani6666/tictactoe-ui
 sudo docker build ./Router -t dani6666/tictactoe-router
 
 
-# docker push dani6666/tictactoe-api 
+# docker push dani6666/tictactoe-api  
 # docker push dani6666/tictactoe-ui 
 # docker push dani6666/tictactoe-proxy 
